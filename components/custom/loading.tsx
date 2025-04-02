7611df3f9c741b521be83e6fd9990f75a54bce67
srgeddes <rileygeddes22@gmail.com>
2025-04-01 20:18:17 -0400

    fix

--- components/custom/loading.tsx
+++
@@ -1,9 +0,0 @@
-import AnimatedBars from "./animated_bars";
-
-export default function Loading() {
-	return (
-		<div className="flex items-center justify-center h-100">
-			<AnimatedBars scale={5} />
-		</div>
-	);
-}
